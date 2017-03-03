ae826fa647eb20f9be236fa89723101a30248420
Leo Jerez <contact@leojerez.com>
2017-03-03 08:36:11 -0500

    final demo version

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets
+{
+    public class PlayerSetup : NetworkBehaviour
+    {
+        [SerializeField]
+        private Behaviour[] _componentsToDisable;
+
+        private Camera sceneCamera;
+
+        void Start()
+        {
+            // Disable all comopnents if it is not the local player
+            if (!isLocalPlayer)
+            {
+                for (int i = 0; i < _componentsToDisable.Length; i++)
+                {
+                    _componentsToDisable[i].enabled = false;
+                }
+            }
+            else
+            {
+                // If it IS the local player, disable the main scene camera
+                sceneCamera = Camera.main;
+                if( sceneCamera != null)
+                    sceneCamera.gameObject.SetActive(false);
+            }
+        }
+
+
+        void OnDisable()
+        {
+            if (sceneCamera != null)
+                sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
